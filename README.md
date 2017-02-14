f1823c75c49a4122ac0f4bc4d63f2c856517a914
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2017-02-14 16:32:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Modular OAI-PMH server [![NPM Version](https://img.shields.io/npm/v/oai-pmh-server.svg)](https://npmjs.org/package/oai-pmh-server) [![Build Status](https://travis-ci.org/natlibfi/oai-pmh-server.svg)](https://travis-ci.org/natlibfi/oai-pmh-server) [![Test Coverage](https://codeclimate.com/github/NatLibFi/oai-pmh-server/badges/coverage.svg)](https://codeclimate.com/github/NatLibFi/oai-pmh-server/coverage)
+
+Modular OAI-PMH server
+
+## License and copyright
+
+Copyright (c) 2017 **University Of Helsinki (The National Library Of Finland)**
+
+This project's source code is licensed under the terms of **GNU Affero General Public License Version 3** or any later version.
---
+++ test/index.spec.js
@@ -0,0 +1,36 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file. 
+*
+* Modular OAI-PMH server
+*
+* Copyright (C) 2017 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of oai-pmh-server
+*
+* oai-pmh-server program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* oai-pmh-server is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+/* eslint-disable no-undef, max-nested-callbacks, no-unused-expressions */
+
+'use strict';
+
+import {expect} from 'chai';
+import * as testContext from '../source/index';
+
+describe.skip('index');
